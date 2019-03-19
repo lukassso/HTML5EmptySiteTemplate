b5bedef606b198807e3cab7b9461a9fdb8ccd962
lukasso <lukasz.koladko@gmail.com>
2019-03-19 13:27:43 +0100

    typo

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ circle.addEventListener("click", function() {
   }
   hide = !hide;
 }, false);
-git
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.set(3, 5, 8).setLength(15);
